ccf891a4e954dab3933bb2efddec870ca9098143
mayur-argade <argademayur2002@gmail.com>
2023-09-05 05:06:20 +0530

    date placeholder changed

--- client/src/pages/Events/Events.jsx
+++ client/src/pages/Events/Events.jsx
@@ -219,7 +219,7 @@ const Events = () => {
                         <div className="datepicker">
                             <div className="p-4">
                                 <label htmlFor="date"></label>
-                                <input className='bg-gray-50 border border-gray-300 text-black text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-36 md:w-40  p-2  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500' for='date' type="date" />
+                                <input className='bg-gray-50 border border-gray-300 text-black placeholder-gray-500 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-36 md:w-40  p-2  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500' for='date' type="date" />
                             </div>
                         </div>
                     </div>
