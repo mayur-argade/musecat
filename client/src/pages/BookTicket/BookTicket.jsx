1a0ffc53ab256b9e360ec0dae4859bc75aa916b4
mayur-argade <argademayur2002@gmail.com>
2023-09-12 02:00:36 +0530

    removed tostify

--- client/src/pages/BookTicket/BookTicket.jsx
+++ client/src/pages/BookTicket/BookTicket.jsx
@@ -6,8 +6,7 @@ import { Link, useNavigate } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 import { ClientBookTicket } from '../../http/index'
 import { setEvent } from '../../store/eventSlice'
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+
 
 const BookTicket = () => {
     document.title = 'Book Ticket'
