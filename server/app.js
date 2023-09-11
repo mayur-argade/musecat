4acc9922bcb9d6818e45d0d19839b452ab4db5c6
mayur-argade <argademayur2002@gmail.com>
2023-09-12 02:39:49 +0530

    changed cors property

--- server/app.js
+++ server/app.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cookieParser())
 
 // cors middleware
-app.use(cors({origin: true, credentials: true}))
+app.use(cors({ credentials: true, origin: true, exposedHeaders: ["Set-Cookie"] }))
 
 // import all routes here
 const auth = require('./routes/AuthRoutes')
