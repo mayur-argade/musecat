94a2fa23f6260db825d9900e52229b55193115fb
mayur-argade <argademayur2002@gmail.com>
2024-01-27 15:10:46 +0530

    login page wordings

--- client/src/pages/Login/VendorLogin.jsx
+++ client/src/pages/Login/VendorLogin.jsx
@@ -158,7 +158,7 @@ const VendorLogin = () => {
                     <button onClick={() => navigate("/vendor/signup")} type="button" class="text-gray-900 bg-white hover:bg-gray-100 border border-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-600 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 mr-2 mb-2">
                         <img src="/images/icons/email-icon.png" alt="" />
                         <span className='mx-auto text-center'>
-                            Continue with Email
+                            continue for Signup
                         </span>
                     </button>
                 </div>
